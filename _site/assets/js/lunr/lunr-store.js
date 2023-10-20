9aa41e3a03fec5f56869317440c3ee06a1c039dd
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-10-21 00:08:22 +0900

    fix: .gitignore 적용되지 않는 문제 해결
    - git rm -r --cached .

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "[Github Blog] jekyll 설치 시 ERROR: Error installing jekyll 오류 해결",
-        "excerpt":"문제 상황 깃헙 블로그를 위해 jekyll gem을 설치할 때, 아래와 같은 에러가 발생했습니다. ERROR: Error installing jekyll: The last version of sass-embedded (~&gt; 1.54) to support your Ruby &amp; RubyGems was 1.63.6. Try installing it with `gem install sass-embedded -v 1.63.6` and then running the current command again sass-embedded requires...","categories": ["Github_Blog"],
-        "tags": ["Github_Blog","ruby"],
-        "url": "/github_blog/github-blog-errors/",
-        "teaser": null
-      }]
