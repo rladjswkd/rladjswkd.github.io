11121c9179cecfdb54ec69acd883940d4ce1212f
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-10-21 00:03:37 +0900

    2023-10-21 post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Post: Standard",
-        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-standard/",
+        "title": "[Github Blog] jekyll 설치 시 ERROR: Error installing jekyll 오류 해결",
+        "excerpt":"문제 상황 깃헙 블로그를 위해 jekyll gem을 설치할 때, 아래와 같은 에러가 발생했습니다. ERROR: Error installing jekyll: The last version of sass-embedded (~&gt; 1.54) to support your Ruby &amp; RubyGems was 1.63.6. Try installing it with `gem install sass-embedded -v 1.63.6` and then running the current command again sass-embedded requires...","categories": ["Github_Blog"],
+        "tags": ["Github_Blog","ruby"],
+        "url": "/github_blog/github-blog-errors/",
         "teaser": null
       }]
